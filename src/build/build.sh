73c8bbd58821c8b1559aa0fdc599d55b0de2c092
Jürgen Viljaste <j.viljaste@gmail.com>
2014-08-18 21:23:23 +0300

    Update

--- src/build/build.sh
+++ src/build/build.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 
-apt-get install -y git
-apt-get install -y wget
-apt-get install -y libmcrypt-dev
-
-apt-get install build-essential
-
-apt-get build-dep -y php5
-
-git clone git://git.code.sf.net/p/phpfarm/code /opt/phpfarm
-
-cp /opt/build/opt/phpfarm/src/custom-options-5.2.17.sh /opt/phpfarm/src/custom-options-5.2.17.sh
-
-/opt/phpfarm/src/compile.sh 5.2.17
+#apt-get install -y git
+#apt-get install -y wget
+#apt-get install -y libmcrypt-dev
+#
+#apt-get install build-essential
+#
+#apt-get build-dep -y php5
+#
+#git clone git://git.code.sf.net/p/phpfarm/code /opt/phpfarm
+#
+#cp /opt/build/opt/phpfarm/src/custom-options-5.2.17.sh /opt/phpfarm/src/custom-options-5.2.17.sh
+#
+#/opt/phpfarm/src/compile.sh 5.2.17
 
 # ADD ./src/opt/phpfarm/src/custom-options-5.3.28.sh /opt/phpfarm/src/custom-options-5.3.28.sh
 # RUN /opt/phpfarm/src/compile.sh 5.3.28
@@ -23,4 +23,4 @@ cp /opt/build/opt/phpfarm/src/custom-options-5.2.17.sh /opt/phpfarm/src/custom-o
 # ADD ./src/opt/phpfarm/src/custom-options-5.5.14.sh /opt/phpfarm/src/custom-options-5.5.14.sh
 # RUN /opt/phpfarm/src/compile.sh 5.5.14
 
-rm -rf /opt/phpfarm/src
+# rm -rf /opt/phpfarm/src
