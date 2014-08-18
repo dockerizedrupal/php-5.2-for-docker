59bb5e5832693ce8ba306c7c804be4d26f721add
Jürgen Viljaste <j.viljaste@gmail.com>
2014-08-18 23:17:31 +0300

    Update

--- src/build/build.sh
+++ src/build/build.sh
@@ -8,30 +8,18 @@ rm /opt/build/etc/puppet/manifests/build.pp
 
 cp /opt/build/etc/puppet/manifests/run.pp /etc/puppet/manifests/run.pp
 
-apt-get install -y git
-apt-get install -y build-essential
-apt-get install -y libxml2-dev
-apt-get install -y libssl-dev
-apt-get install -y libbz2-dev
-apt-get install -y libcurl4-gnutls-dev
-apt-get install -y libpng12-dev
-apt-get install -y libmcrypt-dev
-apt-get install -y libmhash-dev
-apt-get install -y libmysqlclient-dev
-apt-get install -y libpspell-dev
-
 git clone git://git.code.sf.net/p/phpfarm/code /opt/phpfarm
 
-cp /opt/build/opt/phpfarm/src/custom-options-5.2.17.sh /opt/phpfarm/src/custom-options-5.2.17.sh
-/opt/phpfarm/src/compile.sh 5.2.17
+#cp /opt/build/opt/phpfarm/src/custom-options-5.2.17.sh /opt/phpfarm/src/custom-options-5.2.17.sh
+#/opt/phpfarm/src/compile.sh 5.2.17
+#
+#cp /opt/build/opt/phpfarm/src/custom-options-5.3.28.sh /opt/phpfarm/src/custom-options-5.3.28.sh
+#/opt/phpfarm/src/compile.sh 5.3.28
+#
+#cp /opt/build/opt/phpfarm/src/custom-options-5.4.30.sh /opt/phpfarm/src/custom-options-5.4.30.sh
+#/opt/phpfarm/src/compile.sh 5.4.30
+#
+#cp /opt/build/opt/phpfarm/src/custom-options-5.5.14.sh /opt/phpfarm/src/custom-options-5.5.14.sh
+#/opt/phpfarm/src/compile.sh 5.5.14
 
-cp /opt/build/opt/phpfarm/src/custom-options-5.3.28.sh /opt/phpfarm/src/custom-options-5.3.28.sh
-/opt/phpfarm/src/compile.sh 5.3.28
-
-cp /opt/build/opt/phpfarm/src/custom-options-5.4.30.sh /opt/phpfarm/src/custom-options-5.4.30.sh
-/opt/phpfarm/src/compile.sh 5.4.30
-
-cp /opt/build/opt/phpfarm/src/custom-options-5.5.14.sh /opt/phpfarm/src/custom-options-5.5.14.sh
-/opt/phpfarm/src/compile.sh 5.5.14
-
-rm -rf /opt/phpfarm/src
+#rm -rf /opt/phpfarm/src
--- src/build/etc/puppet/manifests/build.pp
+++ src/build/etc/puppet/manifests/build.pp
@@ -1,5 +1,23 @@
 node default {
-  include apt
+  class { '::apt':
+    always_apt_update => true,
+  }
+
+  package {[
+      'git',
+      'build-essential',
+      'libxml2-dev',
+      'libssl-dev',
+      'libbz2-dev',
+      'libcurl4-gnutls-dev',
+      'libpng12-dev',
+      'libmcrypt-dev',
+      'libmhash-dev',
+      'libmysqlclient-dev',
+      'libpspell-dev'
+    ]:
+    ensure  => 'installed'
+  }
 
   apt::source { 'non-free':
     location => 'http://ftp.us.debian.org/debian',
