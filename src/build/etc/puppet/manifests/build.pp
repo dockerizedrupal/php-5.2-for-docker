99aba151b70da4b004a9341251573a5641d680ba
Jürgen Viljaste <j.viljaste@gmail.com>
2014-08-18 23:17:43 +0300

    Update

--- src/build/etc/puppet/manifests/build.pp
+++ src/build/etc/puppet/manifests/build.pp
@@ -18,10 +18,4 @@ node default {
     ]:
     ensure  => 'installed'
   }
-
-  apt::source { 'non-free':
-    location => 'http://ftp.us.debian.org/debian',
-    release => 'stable',
-    repos => 'non-free'
-  }
 }
