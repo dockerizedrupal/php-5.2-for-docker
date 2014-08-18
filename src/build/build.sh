7a6555d8ebaf796d43e2667d3dd9a4a8a7fc674a
Jürgen Viljaste <j.viljaste@gmail.com>
2014-08-18 22:07:37 +0300

    Update

--- src/build/build.sh
+++ src/build/build.sh
@@ -25,13 +25,13 @@ git clone git://git.code.sf.net/p/phpfarm/code /opt/phpfarm
 cp /opt/build/opt/phpfarm/src/custom-options-5.2.17.sh /opt/phpfarm/src/custom-options-5.2.17.sh
 /opt/phpfarm/src/compile.sh 5.2.17
 
-cp /opt/build/src/opt/phpfarm/src/custom-options-5.3.28.sh /opt/phpfarm/src/custom-options-5.3.28.sh
+cp /opt/build/opt/phpfarm/src/custom-options-5.3.28.sh /opt/phpfarm/src/custom-options-5.3.28.sh
 /opt/phpfarm/src/compile.sh 5.3.28
 
-cp /opt/build/src/opt/phpfarm/src/custom-options-5.4.30.sh /opt/phpfarm/src/custom-options-5.4.30.sh
+cp /opt/build/opt/phpfarm/src/custom-options-5.4.30.sh /opt/phpfarm/src/custom-options-5.4.30.sh
 /opt/phpfarm/src/compile.sh 5.4.30
 
-cp /opt/build/src/opt/phpfarm/src/custom-options-5.5.14.sh /opt/phpfarm/src/custom-options-5.5.14.sh
+cp /opt/build/opt/phpfarm/src/custom-options-5.5.14.sh /opt/phpfarm/src/custom-options-5.5.14.sh
 /opt/phpfarm/src/compile.sh 5.5.14
 
 rm -rf /opt/phpfarm/src
