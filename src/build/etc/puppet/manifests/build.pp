5c18a9ff9978e003ea64c3df95e1b938c784e536
Jürgen Viljaste <j.viljaste@gmail.com>
2014-08-19 08:03:00 +0300

    Update

--- src/build/etc/puppet/manifests/build.pp
+++ src/build/etc/puppet/manifests/build.pp
@@ -2,11 +2,12 @@ node default {
   file { '/opt/run.sh':
     ensure => present,
     source => '/tmp/build/run.sh',
+    mode => 755
   }
 
   file { '/etc/puppet/manifests/run.pp':
     ensure => present,
-    source => '/tmp/build/etc/puppet/manifests/run.pp',
+    source => '/tmp/build/etc/puppet/manifests/run.pp'
   }
 
   class { 'apt':
