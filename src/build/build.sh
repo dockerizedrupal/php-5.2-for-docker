b37f04f859daa2a8546dee815fb04391691a7785
Jürgen Viljaste <j.viljaste@gmail.com>
2014-08-19 07:57:24 +0300

    Update

--- src/build/build.sh
+++ src/build/build.sh
@@ -17,5 +17,3 @@ cp /tmp/build/opt/phpfarm/src/custom-options-5.4.30.sh /opt/phpfarm/src/custom-o
 
 cp /tmp/build/opt/phpfarm/src/custom-options-5.5.14.sh /opt/phpfarm/src/custom-options-5.5.14.sh
 /opt/phpfarm/src/compile.sh 5.5.14
-
-#rm -rf /opt/phpfarm/src
