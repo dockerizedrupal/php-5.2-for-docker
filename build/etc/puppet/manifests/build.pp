68041589542e0aa823b427676d450e66ffc1ce9d
Jürgen Viljaste <j.viljaste@gmail.com>
2014-08-30 14:03:39 +0300

    Update

--- build/etc/puppet/manifests/build.pp
+++ build/etc/puppet/manifests/build.pp
@@ -1,3 +1,112 @@
+class packages {
+  package {[
+    'git',
+    'build-essential',
+    'libxml2-dev',
+    'libssl-dev',
+    'libbz2-dev',
+    'libcurl4-gnutls-dev',
+    'libpng12-dev',
+    'libmcrypt-dev',
+    'libmhash-dev',
+    'libmysqlclient-dev',
+    'libpspell-dev'
+  ]:
+    ensure => present
+  }
+}
+
+class phpfarm {
+  exec { 'git clone git://git.code.sf.net/p/phpfarm/code phpfarm':
+    cwd => '/opt',
+    path => ['/usr/bin']
+  }
+}
+
+class php52 {
+  include phpfarm
+
+  file { '/opt/phpfarm/src/custom-options-5.2.17.sh':
+    ensure => present,
+    source => '/tmp/build/opt/phpfarm/src/custom-options-5.2.17.sh',
+    mode => 755,
+    require => Class['phpfarm']
+  }
+
+  exec { '/opt/phpfarm/src/compile.sh 5.2.17':
+    timeout => 0,
+    require => File['/opt/phpfarm/src/custom-options-5.2.17.sh']
+  }
+}
+
+class php53 {
+  include phpfarm
+
+  file { '/opt/phpfarm/src/custom-options-5.3.28.sh':
+    ensure => present,
+    source => '/tmp/build/opt/phpfarm/src/custom-options-5.3.28.sh',
+    mode => 755,
+    require => Class['phpfarm']
+  }
+
+  exec { '/opt/phpfarm/src/compile.sh 5.3.28':
+    timeout => 0,
+    require => File['/opt/phpfarm/src/custom-options-5.3.28.sh']
+  }
+}
+
+class php54 {
+  include phpfarm
+
+  file { '/opt/phpfarm/src/custom-options-5.4.30.sh':
+    ensure => present,
+    source => '/tmp/build/opt/phpfarm/src/custom-options-5.4.30.sh',
+    mode => 755,
+    require => Class['phpfarm']
+  }
+
+  exec { '/opt/phpfarm/src/compile.sh 5.4.30':
+    timeout => 0,
+    require => File['/opt/phpfarm/src/custom-options-5.4.30.sh']
+  }
+}
+
+class php55 {
+  include phpfarm
+
+  file { '/opt/phpfarm/src/custom-options-5.5.14.sh':
+    ensure => present,
+    source => '/tmp/build/opt/phpfarm/src/custom-options-5.4.14.sh',
+    mode => 755,
+    require => Class['phpfarm']
+  }
+
+  exec { '/opt/phpfarm/src/compile.sh 5.5.14':
+    timeout => 0,
+    require => File['/opt/phpfarm/src/custom-options-5.5.14.sh']
+  }
+}
+
+class php {
+  include php52
+  include php53
+  include php54
+  include php55
+
+  Class['php52'] -> Class['php53'] -> Class['php54'] -> Class['php55']
+
+  file { '/etc/profile.d/phpfarm.sh':
+    ensure => present,
+    source => '/tmp/build/etc/profile.d/phpfarm.sh',
+    mode => 755,
+    require => Class['php55']
+  }
+
+  exec { '/bin/bash -c "source /etc/profile.d/phpfarm.sh && switch-phpfarm 5.5.14"':
+    require => File['/etc/profile.d/phpfarm.sh']
+  }
+}
+
 node default {
   file { '/run.sh':
     ensure => present,
@@ -5,19 +114,13 @@ node default {
     mode => 755
   }
 
-  package {[
-      'git',
-      'build-essential',
-      'libxml2-dev',
-      'libssl-dev',
-      'libbz2-dev',
-      'libcurl4-gnutls-dev',
-      'libpng12-dev',
-      'libmcrypt-dev',
-      'libmhash-dev',
-      'libmysqlclient-dev',
-      'libpspell-dev'
-    ]:
-    ensure  => present
+  include packages
+  include php
+
+  Class['packages'] -> Class['php']
+
+  exec { 'apt-get update':
+    path => ['/usr/bin'],
+    before => Class['packages']
   }
 }
