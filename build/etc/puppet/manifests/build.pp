5aee3e6b104ec6850ebb3e257936db9f030c3c1d
Jürgen Viljaste <j.viljaste@gmail.com>
2014-09-02 01:39:32 +0300

    Update

--- build/etc/puppet/manifests/build.pp
+++ build/etc/puppet/manifests/build.pp
@@ -12,7 +12,8 @@ class packages {
     'libmhash-dev',
     'libmysqlclient-dev',
     'libpspell-dev',
-    'autoconf'
+    'autoconf',
+    'libcloog-ppl0'
   ]:
     ensure => present
   }
@@ -38,13 +39,13 @@ class php_5217_supervisor {
 class php_5217_extension_xdebug {
   include php_5217
 
-  exec { 'pear download pecl/xdebug':
+  exec { 'pear download pecl/xdebug-2.2.5':
     cwd => '/tmp',
     path => ['/opt/phpfarm/inst/php-5.2.17/bin'],
     require => Class['php_5217']
   }
 
-  exec { 'tar xzvf xdebug-2.2.5.tgz':
+  exec { 'tar xzf xdebug-2.2.5.tgz':
     cwd => '/tmp',
     path => ['/bin'],
     require => Exec['pear download pecl/xdebug']
@@ -68,10 +69,73 @@ class php_5217_extension_xdebug {
   }
 }
 
+class php_5217_libmemcached {
+  include php_5217_extension_memcached
+
+  exec { 'wget https://launchpad.net/libmemcached/1.0/0.53/+download/libmemcached-0.53.tar.gz':
+    cwd => '/tmp',
+    path => ['/usr/bin/'],
+    before => Class['php_5217_extension_memcached']
+  }
+
+  exec { 'tar xzf libmemcached-0.53.tar.gz':
+    cwd => '/tmp',
+    path => ['/bin'],
+    require => Exec['wget https://launchpad.net/libmemcached/1.0/0.53/+download/libmemcached-0.53.tar.gz']
+  }
+
+  exec { '/bin/bash -l -c "cd /tmp/libmemcached-0.53 && ./configure"':
+    require => Exec['tar xzf libmemcached-0.53.tar.gz']
+  }
+
+  exec { '/bin/bash -l -c "cd /tmp/libmemcached-0.53 && make"':
+    require => Exec['/bin/bash -l -c "cd libmemcached-0.53 && ./configure"']
+  }
+
+  exec { '/bin/bash -l -c "cd /tmp/libmemcached-0.53 && make install"':
+    require => Exec['/bin/bash -l -c "cd /tmp/libmemcached-0.53 && make"']
+  }
+}
+
+class php_5217_extension_memcached {
+  include php_5217
+  include php_5217_libmemcached
+
+  exec { 'pear download pecl/memcached-1.0.2':
+    cwd => '/tmp',
+    path => ['/opt/phpfarm/inst/php-5.2.17/bin'],
+    require => Class['php_5217']
+  }
+
+  exec { 'tar xzf memcached-1.0.2.tgz':
+    cwd => '/tmp',
+    path => ['/bin'],
+    require => Exec['pear download pecl/memcached-1.0.2']
+  }
+
+  exec { '/opt/phpfarm/inst/bin/phpize-5.2.17':
+    cwd => '/tmp/memcached-1.0.2',
+    require => Exec['tar xzvf memcached-1.0.2.tgz']
+  }
+
+  exec { '/bin/bash -l -c "cd /tmp/memcached-1.0.2 && ./configure --with-php-config=/opt/phpfarm/inst/bin/php-config-5.2.17"':
+    require => Exec['/opt/phpfarm/inst/bin/phpize-5.2.17']
+  }
+
+  exec { '/bin/bash -l -c "cd /tmp/memcached-1.0.2 && make"':
+    require => Exec['/bin/bash -l -c "cd /tmp/memcached-1.0.2 && ./configure --with-php-config=/opt/phpfarm/inst/bin/php-config-5.2.17"']
+  }
+
+  exec { '/bin/bash -l -c "cd /tmp/memcached-1.0.2 && make install"':
+    require => Exec['/bin/bash -l -c "cd /tmp/memcached-1.0.2 && make"']
+  }
+}
+
 class php_5217 {
   include phpfarm
   include php_5217_supervisor
   include php_5217_extension_xdebug
+  include php_5217_extension_memcached
 
   file { '/opt/phpfarm/src/custom-options-5.2.17.sh':
     ensure => present,
@@ -88,6 +152,13 @@ class php_5217 {
   file { '/opt/phpfarm/inst/php-5.2.17/etc/php-fpm.conf':
     ensure => present,
     source => '/tmp/build/opt/phpfarm/inst/php-5.2.17/etc/php-fpm.conf',
+    mode => 644,
+    require => Exec['/opt/phpfarm/src/compile.sh 5.2.17']
+  }
+
+  file { '/opt/phpfarm/inst/php-5.2.17/lib/php.ini':
+    ensure => present,
+    source => '/tmp/build/opt/phpfarm/inst/php-5.2.17/lib/php.ini',
     mode => 644,
     require => Exec['/opt/phpfarm/src/compile.sh 5.2.17']
   }
