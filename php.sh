0e3ea92b771bbe1a0cb6c08fcc388bdfa0d25d9b
Jürgen Viljaste <j.viljaste@gmail.com>
2014-09-05 00:11:26 +0300

    Update

--- php.sh
+++ php.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
-sudo docker run --rm -i -t -v $(pwd):/src simpledrupalcloud/php:5.2.17 "${@}"
+if [ -n "${1}" ]; then
+  sudo docker run --rm -i -t -v $(pwd):/src simpledrupalcloud/php:5.2.17 "${@}"
+else
+  sudo docker run --name php --net host --volumes-from apache -d simpledrupalcloud/php:5.2.17
+fi
