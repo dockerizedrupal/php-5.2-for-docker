4ca9f99b228c965423d33856380b6391c9860821
Jürgen Viljaste <j.viljaste@gmail.com>
2014-09-09 22:07:11 +0300

    Update

--- build/etc/puppet/manifests/build.pp
+++ build/etc/puppet/manifests/build.pp
@@ -1,20 +1,20 @@
 class packages {
   package {[
-    'git',
-    'curl',
-    'build-essential',
-    'libxml2-dev',
-    'libssl-dev',
-    'libbz2-dev',
-    'libcurl4-gnutls-dev',
-    'libpng12-dev',
-    'libmcrypt-dev',
-    'libmhash-dev',
-    'libmysqlclient-dev',
-    'libpspell-dev',
-    'autoconf',
-    'libcloog-ppl0'
-  ]:
+      'git',
+      'curl',
+      'build-essential',
+      'libxml2-dev',
+      'libssl-dev',
+      'libbz2-dev',
+      'libcurl4-gnutls-dev',
+      'libpng12-dev',
+      'libmcrypt-dev',
+      'libmhash-dev',
+      'libmysqlclient-dev',
+      'libpspell-dev',
+      'autoconf',
+      'libcloog-ppl0'
+    ]:
     ensure => present
   }
 }
