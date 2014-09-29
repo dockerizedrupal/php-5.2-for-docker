5a993340f54760f156cb1b7d7b32e4893a76de14
Jürgen Viljaste <j.viljaste@gmail.com>
2014-09-29 10:37:34 +0300

    Update

--- build/phpfarm/src/custom-options-5.2.17.sh
+++ build/phpfarm/src/custom-options-5.2.17.sh
@@ -1,7 +1,6 @@
 configoptions="--with-libdir=/lib/x86_64-linux-gnu \
   --enable-apc \
   --enable-memcached \
-  --enable-xdebug \
   --enable-fpm \
   --enable-fastcgi \
   --enable-force-cgi-redirect \
