6039105c5f78256633eec588faf0188f519f735c
Jürgen Viljaste <j.viljaste@gmail.com>
2014-09-29 10:33:21 +0300

    Update

--- build/etc/puppet/manifests/build.pp
+++ build/etc/puppet/manifests/build.pp
@@ -40,34 +40,28 @@ class php_supervisor {
 class php_extension_xdebug {
   include php
 
-  exec { 'wget http://xdebug.org/files/xdebug-2.2.5.tgz':
+  exec { 'git clone https://github.com/xdebug/xdebug.git':
     cwd => '/tmp',
     path => ['/usr/bin'],
     require => Class['php']
   }
 
-  exec { 'tar xzf xdebug-2.2.5.tgz':
-    cwd => '/tmp',
-    path => ['/bin'],
-    require => Exec['wget http://xdebug.org/files/xdebug-2.2.5.tgz']
-  }
-
   exec { 'phpize-5.2.17 xdebug':
     command => '/phpfarm/inst/bin/phpize-5.2.17',
-    cwd => '/tmp/xdebug-2.2.5',
-    require => Exec['tar xzf xdebug-2.2.5.tgz']
+    cwd => '/tmp/xdebug',
+    require => Exec['git clone https://github.com/xdebug/xdebug.git']
   }
 
-  exec { '/bin/bash -l -c "cd /tmp/xdebug-2.2.5 && ./configure --with-php-config=/phpfarm/inst/bin/php-config-5.2.17"':
+  exec { '/bin/bash -l -c "cd /tmp/xdebug && ./configure --with-php-config=/phpfarm/inst/bin/php-config-5.2.17"':
     require => Exec['phpize-5.2.17 xdebug']
   }
 
-  exec { '/bin/bash -l -c "cd /tmp/xdebug-2.2.5 && make"':
-    require => Exec['/bin/bash -l -c "cd /tmp/xdebug-2.2.5 && ./configure --with-php-config=/phpfarm/inst/bin/php-config-5.2.17"']
+  exec { '/bin/bash -l -c "cd /tmp/xdebug && make"':
+    require => Exec['/bin/bash -l -c "cd /tmp/xdebug && ./configure --with-php-config=/phpfarm/inst/bin/php-config-5.2.17"']
   }
 
-  exec { '/bin/bash -l -c "cd /tmp/xdebug-2.2.5 && make install"':
-    require => Exec['/bin/bash -l -c "cd /tmp/xdebug-2.2.5 && make"']
+  exec { '/bin/bash -l -c "cd /tmp/xdebug && make install"':
+    require => Exec['/bin/bash -l -c "cd /tmp/xdebug && make"']
   }
 }
 
