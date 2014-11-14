cac4a772066a1dd467c0a6be418d4793e487ceb5
Jürgen Viljaste <j.viljaste@gmail.com>
2014-11-14 15:58:56 +0200

    Update

--- src/run/modules/mysql/manifests/init.pp
+++ src/run/modules/mysql/manifests/init.pp
@@ -1,6 +1,10 @@
 class mysql {
   require mysql::packages
 
+  file { '/var/run/mysqld':
+    ensure => directory
+  }
+
   file { '/etc/supervisor/conf.d/mysql.conf':
     ensure => present,
     content => template('mysql/mysql.conf.erb'),
