845417331d09f0f006c0f2501099639badf53310
Jürgen Viljaste <j.viljaste@gmail.com>
2014-11-21 11:55:52 +0200

    Update

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# docker-php-5.2-dev
+# docker-php
 
 A [Docker](https://docker.com/) container for [PHP](http://php.net/) version 5.2.17 that runs PHP in FPM (FastCGI Process Manager) mode.
 
@@ -14,7 +14,7 @@ Using the `docker` command:
       -v /var/www:/var/www \
       simpledrupalcloud/data:dev
 
-    CONTAINER="php52" && sudo docker run \
+    CONTAINER="php" && sudo docker run \
       --name "${CONTAINER}" \
       -h "${CONTAINER}" \
       -p 9000:9000 \
@@ -44,16 +44,16 @@ Using the `fig` command
     <IfModule mod_fastcgi.c>
       AddHandler php .php
 
-      Alias /php52 /var/www/php52
-      FastCgiExternalServer /var/www/php52 -host 127.0.0.1:9000 -idle-timeout 300 -pass-header Authorization
+      Alias /php /var/www/php
+      FastCgiExternalServer /var/www/php -host 127.0.0.1:9000 -idle-timeout 300 -pass-header Authorization
 
-      <Location /php52>
+      <Location /php>
         Order deny,allow
         Deny from all
         Allow from env=REDIRECT_STATUS
       </Location>
 
-      Action php /php52
+      Action php /php
     </IfModule>
 
 ## License
--- src/run.sh
+++ src/run.sh
@@ -1,32 +1,7 @@
 #!/usr/bin/env bash
 
-dnsLookup() {
-  echo "$(host "${1}" | awk '/has address/ { print $4 }')"
-}
-
-if [ -z "${SSMTP_PORT_25_TCP}" ] && [ -n "${SSMTP_PORT_25_TCP_ADDR}" ] && [ -n "${SSMTP_PORT_25_TCP_PORT}" ]; then
-  IP="$(dnsLookup "${SSMTP_PORT_25_TCP_ADDR}")"
-
-  if [ ! -z "${IP}" ]; then
-    SSMTP_PORT_25_TCP_ADDR="${IP}"
-  fi
-
-  SSMTP_PORT_25_TCP="tcp://${SSMTP_PORT_25_TCP_ADDR}:${SSMTP_PORT_25_TCP_PORT}"
-fi
-
-export FACTER_SSMTP_PORT_25_TCP="$(echo "${SSMTP_PORT_25_TCP}" | sed 's/tcp:\/\///')"
-
-if [ -z "${MYSQLD_PORT_3306_TCP}" ] && [ -n "${MYSQLD_PORT_3306_TCP_ADDR}" ] && [ -n "${MYSQLD_PORT_3306_TCP_PORT}" ]; then
-  IP="$(dnsLookup "${MYSQLD_PORT_3306_TCP_ADDR}")"
-
-  if [ ! -z "${IP}" ]; then
-    MYSQLD_PORT_3306_TCP_ADDR="${IP}"
-  fi
-
-  MYSQLD_PORT_3306_TCP="tcp://${MYSQLD_PORT_3306_TCP_ADDR}:${MYSQLD_PORT_3306_TCP_PORT}"
-fi
-
-export FACTER_MYSQLD_PORT_3306_TCP="$(echo "${MYSQLD_PORT_3306_TCP}" | sed 's/tcp:\/\///')"
+export FACTER_SSMTP_HOST="$(echo "${SSMTP_PORT}" | sed 's/tcp:\/\///')"
+export FACTER_MYSQLD_HOST="$(echo "${MYSQLD_PORT}" | sed 's/tcp:\/\///')"
 
 puppet apply --modulepath=/src/run/modules /src/run/run.pp
 
