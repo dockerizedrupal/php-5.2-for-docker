e9732028b10964e59b49019ef3d37a33f10a7c92
Jürgen Viljaste <j.viljaste@gmail.com>
2014-11-23 02:34:42 +0200

    Update

--- src/build/modules/php/manifests/extension/memcached.pp
+++ src/build/modules/php/manifests/extension/memcached.pp
@@ -28,36 +28,36 @@ class php::extension::memcached {
     require => Exec['/bin/bash -l -c "cd /tmp/libmemcached-1.0.18 && make"']
   }
 
-  file { '/tmp/memcached-2.2.0.tgz':
+  file { '/tmp/memcached-2.1.0.tgz':
     ensure => present,
-    source => 'puppet:///modules/php/tmp/memcached-2.2.0.tgz',
+    source => 'puppet:///modules/php/tmp/memcached-2.1.0.tgz',
     require => Exec['/bin/bash -l -c "cd /tmp/libmemcached-1.0.18 && make install"']
   }
 
-  exec { 'tar xzf memcached-2.2.0.tgz':
+  exec { 'tar xzf memcached-2.1.0.tgz':
     cwd => '/tmp',
     path => ['/bin'],
-    require => File['/tmp/memcached-2.2.0.tgz']
+    require => File['/tmp/memcached-2.1.0.tgz']
   }
 
   exec { 'phpize-5.2.17 memcached':
     command => '/phpfarm/inst/bin/phpize-5.2.17',
-    cwd => '/tmp/memcached-2.2.0',
-    require => Exec['tar xzf memcached-2.2.0.tgz']
+    cwd => '/tmp/memcached-2.1.0',
+    require => Exec['tar xzf memcached-2.1.0.tgz']
   }
 
-  exec { '/bin/bash -l -c "cd /tmp/memcached-2.2.0 && ./configure --with-php-config=/phpfarm/inst/bin/php-config-5.2.17 --enable-memcached-igbinary"':
+  exec { '/bin/bash -l -c "cd /tmp/memcached-2.1.0 && ./configure --with-php-config=/phpfarm/inst/bin/php-config-5.2.17 --enable-memcached-igbinary"':
     timeout => 0,
     require => Exec['phpize-5.2.17 memcached']
   }
 
-  exec { '/bin/bash -l -c "cd /tmp/memcached-2.2.0 && make"':
+  exec { '/bin/bash -l -c "cd /tmp/memcached-2.1.0 && make"':
     timeout => 0,
-    require => Exec['/bin/bash -l -c "cd /tmp/memcached-2.2.0 && ./configure --with-php-config=/phpfarm/inst/bin/php-config-5.2.17 --enable-memcached-igbinary"']
+    require => Exec['/bin/bash -l -c "cd /tmp/memcached-2.1.0 && ./configure --with-php-config=/phpfarm/inst/bin/php-config-5.2.17 --enable-memcached-igbinary"']
   }
 
-  exec { '/bin/bash -l -c "cd /tmp/memcached-2.2.0 && make install"':
+  exec { '/bin/bash -l -c "cd /tmp/memcached-2.1.0 && make install"':
     timeout => 0,
-    require => Exec['/bin/bash -l -c "cd /tmp/memcached-2.2.0 && make"']
+    require => Exec['/bin/bash -l -c "cd /tmp/memcached-2.1.0 && make"']
   }
 }
