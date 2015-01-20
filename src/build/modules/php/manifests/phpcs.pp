532ecb3cad212d866645124e2ff54584dc6cc61e
Jürgen Viljaste <j.viljaste@gmail.com>
2015-01-20 17:58:26 +0200

    Update

--- src/build/modules/php/manifests/phpcs.pp
+++ src/build/modules/php/manifests/phpcs.pp
@@ -1,20 +1,20 @@
 class php::phpcs {
   require php
 
-  file { '/tmp/PHP_CodeSniffer-2.1.0.tgz':
+  file { '/tmp/PHP_CodeSniffer-1.5.6.tar.gz':
     ensure => present,
-    source => 'puppet:///modules/php/tmp/PHP_CodeSniffer-2.1.0.tgz'
+    source => 'puppet:///modules/php/tmp/PHP_CodeSniffer-1.5.6.tar.gz'
   }
 
-  exec { 'tar xzf PHP_CodeSniffer-2.1.0.tgz':
+  exec { 'tar xzf PHP_CodeSniffer-1.5.6.tar.gz':
     cwd => '/tmp',
     path => ['/bin'],
-    require => File['/tmp/PHP_CodeSniffer-2.1.0.tgz']
+    require => File['/tmp/PHP_CodeSniffer-1.5.6.tar.gz']
   }
 
-  exec { 'mv PHP_CodeSniffer-2.1.0 /opt/PHP_CodeSniffer':
+  exec { 'mv PHP_CodeSniffer-1.5.6 /opt/PHP_CodeSniffer':
     cwd => '/tmp',
     path => ['/bin'],
-    require => Exec['tar xzf PHP_CodeSniffer-2.1.0.tgz']
+    require => Exec['tar xzf PHP_CodeSniffer-1.5.6.tar.gz']
   }
 }
