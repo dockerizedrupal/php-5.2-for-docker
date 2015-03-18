ac8983d246c228759c834132b7add292f153bfb9
Jürgen Viljaste <j.viljaste@gmail.com>
2015-03-18 21:52:28 +0200

    Update

--- src/php/build/modules/php/manifests/phpcs/phpcompatibility.pp
+++ src/php/build/modules/php/manifests/phpcs/phpcompatibility.pp
@@ -10,7 +10,7 @@ class php::phpcs::phpcompatibility {
     require => File['/tmp/PHPCompatibility-master.zip']
   }
 
-  bash_exec { 'mv /tmp/PHPCompatibility-master /usr/local/src/PHP_CodeSniffer/CodeSniffer/Standards/PHPCompatibility':
+  bash_exec { 'mv /tmp/PHPCompatibility-master /usr/local/src/PHP_CodeSniffer_1/CodeSniffer/Standards/PHPCompatibility':
     require => Bash_exec['cd /tmp && unzip PHPCompatibility-master.zip']
   }
 }
