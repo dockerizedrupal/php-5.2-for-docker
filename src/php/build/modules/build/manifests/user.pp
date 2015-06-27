b5b7e79380a4c930838520af804526c2a4a316bb
Jürgen Viljaste <j.viljaste@gmail.com>
2015-06-27 20:30:52 +0300

    Update

--- src/php/build/modules/build/manifests/user.pp
+++ src/php/build/modules/build/manifests/user.pp
@@ -1,5 +1,4 @@
 class build::user {
   bash_exec { "groupadd -o -g $(id -g www-data) container": }
-
   bash_exec { "useradd -m -o -s /bin/bash -u $(id -u www-data) -g $(id -g www-data) container": }
 }
--- src/php/run/modules/run/manifests/drush.pp
+++ src/php/run/modules/run/manifests/drush.pp
@@ -1,4 +1,6 @@
 class run::drush {
+  require run::user
+
   if $drupal_version == '6' {
     file { '/usr/local/bin/drush':
       ensure => link,
@@ -9,5 +11,8 @@ class run::drush {
       ensure => link,
       target => '/usr/local/src/drush5/drush.complete.sh'
     }
+
+    bash_exec { "chown -R -h $user_id /usr/local/src/drush5": }
+    bash_exec { "chgrp -R -h $group_id /usr/local/src/drush5": }
   }
 }
