4a2ce328d2d93d6095f9412d3d73dcd2ba28f7bc
Jürgen Viljaste <j.viljaste@gmail.com>
2016-09-17 21:12:02 +0300

    #32

--- src/php-5.2/build/modules/build/files/usr/local/src/phpfarm/src/custom/options-5.2.17.sh
+++ src/php-5.2/build/modules/build/files/usr/local/src/phpfarm/src/custom/options-5.2.17.sh
@@ -42,6 +42,11 @@ configoptions="--with-libdir=/lib/x86_64-linux-gnu \
 
 cd $srcdir
 
+# https://bugs.php.net/patch-display.php?bug_id=54736&patch=debian_patches_disable_SSLv2_for_openssl_1_0_0.patch&revision=1305414559&download=1
 cat /src/php-5.2/build/modules/build/files/tmp/debian_patches_disable_SSLv2_for_openssl_1_0_0.patch.patch.txt | patch -p1
+
+# https://mail.gnome.org/archives/xml/2012-August/txtbgxGXAvz4N.txt
 cat /src/php-5.2/build/modules/build/files/tmp/txtbgxGXAvz4N.txt | patch -p0
+
+# http://php-fpm.org/downloads/php-5.2.17-fpm-0.5.14.diff.gz
 zcat /src/php-5.2/build/modules/build/files/tmp/php-5.2.17-fpm-0.5.14.diff.gz | patch -p1
